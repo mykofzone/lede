aa26e749e85b44314ad4f48237371434079465b3
monw <65690955+monw@users.noreply.github.com>
2020-09-01 22:35:39 +0800

    Update mach-tl-wr810n.c

--- target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr810n.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr810n.c
@@ -26,12 +26,14 @@
 
 #include "common.h"
 #include "dev-gpio-buttons.h"
+#include "dev-ap9x-pci.h"
 #include "dev-eth.h"
 #include "dev-leds-gpio.h"
 #include "dev-m25p80.h"
 #include "dev-usb.h"
 #include "dev-wmac.h"
 #include "machtypes.h"
+#include "pci.h"
 
 #define TL_WR810N_GPIO_SWITCH_B1	0
 #define TL_WR810N_GPIO_SWITCH_B0	1
@@ -98,6 +100,8 @@ static void __init tl_ap143_setup(int lan_mac_offset)
 	ath79_setup_ar933x_phy4_switch(false, false);
 
 	ath79_register_mdio(0, 0x0);
+	
+	ath79_register_pci();
 
 	/* WAN */
 	ath79_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_MII;
@@ -111,7 +115,7 @@ static void __init tl_ap143_setup(int lan_mac_offset)
 	ath79_switch_data.phy4_mii_en = 1;
 	ath79_eth1_data.phy_if_mode = PHY_INTERFACE_MODE_GMII;
 	ath79_eth1_data.duplex = DUPLEX_FULL;
-	ath79_eth1_data.speed = SPEED_1000;
+	ath79_eth1_data.speed = SPEED_100;
 	ath79_switch_data.phy_poll_mask |= BIT(4);
 	ath79_init_mac(ath79_eth1_data.mac_addr, mac, lan_mac_offset);
 	ath79_register_eth(1);
